a5d8d2e854ae1e8fb80695250e1ea133939249ad
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-04-04 16:04:11 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+document.write('Hello, world!');
+var A = "This is a string. ";
+document.write(A);
+
+var RG = "This is content provided by Renato Grizelj";
+window.alert(RG);
+
+document.write(" His mom said, \"What\'s wrong with you?\"");
+
+document.write(" My name is Renato Grizelj." + 
+" I am a student at the Tech Academy.")
+
+var B = " Concatenated" + " String "
+document.write(B);
+
+
+var Team = "The Warriors", point_guard = "Steph Curry", 
+shooting_guard = "Klay Thompson", power_forward = "Draymond Green", 
+small_forward = "Andrew Wiggins", center = "Kevon Looney";
+
+document.write(shooting_guard.fontcolor('blue'));
+
+document.write(3 + 3); //This is an expression of 3 + 3
