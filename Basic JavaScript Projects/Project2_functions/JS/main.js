81cfbfc2d63defd41c79e59ce4acfa313475ee4e
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-04-05 09:45:51 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
 function My_First_Funtion() {
     var str = "This is the button text!";
-    document.getElementById("Button_Text").innerHTML = str;
+    document.getElementById("Button_Text").innerHTML = str;  //A function to add text after the "Click me!" button is triggered
 }
 
 function myFunction () {
     var sentence = "I am learning";
-    sentence += " a lot from the Tech Academy!";
+    sentence += " a lot from the Tech Academy!";  //A function that uses the += operator
     document.getElementById("Concatenate").innerHTML = sentence;
 }
