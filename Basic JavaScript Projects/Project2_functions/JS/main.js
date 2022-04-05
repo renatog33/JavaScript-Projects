d05f59efc35c31a412199f123a9d7ce145c3ce23
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-04-05 09:42:10 -0700

    OPERATOR ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Funtion() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction () {
+    var sentence = "I am learning";
+    sentence += " a lot from the Tech Academy!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
